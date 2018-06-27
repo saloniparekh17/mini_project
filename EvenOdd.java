8892f1f70ebddf86acdfc594714883480c079d67
saloniparekh17 <40625277+saloniparekh17@users.noreply.github.com>
2018-06-27 16:55:11 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+Class EvenOdd
+{
+	public static void main(String a[])
+	{
+		int n=9;
+		if(n%2==0)
+			System.out.println(n + "is an even number");
+		else
+			System.out.println(n + "is an odd number");
+	}
+}
---
+++ PrintMessage.java
@@ -0,0 +1,13 @@
+class PrintMessage
+{
+	public static void main(String a[])
+	{
+		int num=7;
+		if (num%3 == 0)
+			System.out.println(num+" is divisible by 3");
+		else if(num%5==0)
+			System.out.println(num+" is divisible by 5");
+		else
+			System.out.println(num+" is not divisible by 3 and 5");
+	}
+}
