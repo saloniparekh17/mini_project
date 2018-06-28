0e3044b9c15ea7856269563d7cbd39b7f4b7e334
saloniparekh17 <saloni.p1712@gmail.com>
2018-06-28 12:35:14 +0530

    Adding a new file

---
+++ ArithmeticOperations.java
@@ -0,0 +1,16 @@
+class ArithmeticOpeations
+{
+	public static void main(String a[])
+	{
+		System.out.println("Perform all Arithmetic Operations here ");
+		int num1=8,num2=2;
+		add=num1+num2;
+		sub=num1-num2;
+		mul=num1*num2;
+		div=num1/num2;
+		System.out.println("Addition= "+add);
+		System.out.println("Subtraction= "+sub);
+		System.out.println("Multiplication= "+mul);
+		System.out.println("Division ="+div);
+	}
+}
