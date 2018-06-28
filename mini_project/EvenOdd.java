a8ce38836df23285b9f0c5aace9852b9eb0e38bc
FK1996 <farheenkamal90@gmail.com>
2018-06-28 12:34:54 +0530

    New_Changes From Farheen

---
+++ mini_project/EvenOdd.java
@@ -0,0 +1,15 @@
+
+Class EvenOdd
+{
+	public static void main(String a[])
+	{
+		System.out.println("Output of Preogram is:");
+		int n=20;
+		if(n%2==0)
+			System.out.println(n + "is an even number");
+		else
+			System.out.println(n + "is an odd number");
+	}
+	System.out.println("Changes made by Darshan");
+	System.out.println("Changes made by me again");
+}
